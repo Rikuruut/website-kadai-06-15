d84232488319d3bb8acfcc8fc9073ba4f30ba4ff
Rikuruut <hidakaliku0613@gmail.com>
2026-06-15 02:09:39 +0900

    Delete profile_kadai4 directory

--- profile_kadai4/js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hambuger');
-const navi = document.querySelector('.navi');
-
-hamburger.addEventListener('click', () => {
-    navi.classList.toggle('open');
-});
